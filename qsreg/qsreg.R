85b46973751b7fabcf3a7b65ec32c9eb65744cb4
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2026-03-19 07:30:29 +0700

    qsreg (fields) With (In) R Software

---
+++ qsreg/qsreg.R
@@ -0,0 +1,16 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Quantile or Robust spline regression Use qsreg (fields) With (In) R Software
+install.packages("fields")
+library(fields)
+# Estimation Quantile or Robust spline regression Use qsreg (fields) With (In) R Software
+qsreg = read.csv("https://raw.githubusercontent.com/timbulwidodostp/qsreg/main/qsreg/qsreg.csv",sep = ";")
+qsreg <- qsreg(qsreg$qsreg, qsreg$qsreg_)
+qsreg
+plot(qsreg)
+# Quantile or Robust spline regression Use qsreg (fields) With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
